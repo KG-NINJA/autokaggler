83270347d12884eb8050539f5babc6b84037bdab
KG <fuwafuwow@gmail.com>
2025-09-18 18:24:32 +0900

    Update __init__.py

--- src/autokaggler/__init__.py
+++ src/autokaggler/__init__.py
@@ -1,5 +1,16 @@
-"""AutoKaggler package for automating Kaggle Titanic tutorial pipeline."""
+"""Autokaggler package init."""
 
-from .agent import main
+from .agent import main, build_success_result, AgentResult, TaskInput
+from .data_manager import DataManager, DataMeta
+from .pipeline import TitanicPipeline, TitanicPipelineResult
 
-__all__ = ["main"]
+__all__ = [
+    "main",
+    "build_success_result",
+    "AgentResult",
+    "TaskInput",
+    "DataManager",
+    "DataMeta",
+    "TitanicPipeline",
+    "TitanicPipelineResult",
+]
